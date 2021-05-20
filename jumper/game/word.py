1d03eb4349ca32e66058cc7ec692666340da2508
markvagil <83776097+markvagil@users.noreply.github.com>
2021-05-20 12:36:30 -0500

    Update word.py

--- jumper/game/word.py
+++ jumper/game/word.py
@@ -1,14 +1,15 @@
 import random
 
+
 class Word():
 
     def __init__(self):
-        self.words = ["abruptly", "beekeeper", "blizzard", "cobweb", "flapjack", "haphazard", "jackpot", "syntax", "abstraction", "microwave", "witchcraft", "zombie", "corruption", "jogging", "keyboard"]
+        self.words = ["abruptly", "beekeeper", "blizzard", "cobweb", "flapjack", "haphazard", "jackpot", "syntax",
+                      "abstraction", "microwave", "witchcraft", "zombie", "corruption", "jogging", "keyboard"]
         self.choice = random.randint(0, 14)
         self.the_word = self.words[self.choice]
         self.blanks = []
         self.make_blanks()
-        
 
     def check_letter(self, letter):
         if letter in self.the_word:
@@ -21,9 +22,14 @@ class Word():
         for _ in self.the_word:
             self.blanks.append("_")
         return self.blanks
-    
+
     def replace_blank(self, letter):
         for i in range(0, len(self.the_word)):
             if letter == self.the_word[i]:
-                print (letter)
                 self.blanks[i] = letter
+
+    def check_blanks(self):
+        if "_" in self.blanks:
+            return True
+        else:
+            return False
