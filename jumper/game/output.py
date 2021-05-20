a7cc8a19411fc1dc67701b67cd12c94f304093e8
markvagil <83776097+markvagil@users.noreply.github.com>
2021-05-20 12:36:11 -0500

    Update output.py

--- jumper/game/output.py
+++ jumper/game/output.py
@@ -1,1 +1,16 @@
 
+
+
+class Output:
+
+    def print_jumper(self, strikes, jumper):
+        jumper.print_jumper(strikes)
+
+    def print_blanks(self, word):
+        print(*word.blanks, sep=" ")
+
+    def print_win(self):
+        print("Congrats, you guessed the word!")
+
+    def print_loss(self):
+        print("Sorry, you hit the ground a little too hard!")
