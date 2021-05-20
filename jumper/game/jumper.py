c3407c261ed5538c83ced92d8b3156b020811e37
markvagil <83776097+markvagil@users.noreply.github.com>
2021-05-20 12:35:58 -0500

    Update jumper.py

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -3,19 +3,11 @@
 class Jumper:
 
     def __init__(self):
-        self.par1 = "  ___  "
-        self.par2 = " /___\ "
-        self.par3 = " \   / "
-        self.par4 = "  \ /  "
         self.parachute = ["  ___  ", " /___\ ", " \   / ", "  \ /  "]
 
 
     def print_jumper(self, strikes):
         if strikes < 4:
-            i = 0
-            for i in range(0, strikes):
-                self.parachute.pop(0)
-
             for z in self.parachute:
                 print(z)
 
