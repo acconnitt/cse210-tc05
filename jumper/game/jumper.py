491e234c2cd54b168fd13822b635c18f8c5c673e
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-21 20:12:40 -0400

    Jumper

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,16 +1,15 @@
-
-
 class Jumper:
 
     def __init__(self):
-        self.parachute = ["  ___  ", " /___\ ", " \   / ", "  \ /  "]
-
+        # initilize the jumper class
+        self.parachute = ["  ___  ", " /___\ ",  " \   / ",
+                          "  \ /  "]  # Array that has parachute drawing
 
     def print_jumper(self, strikes):
-        if strikes < 4:
-            for z in self.parachute:
+        if strikes < 4:  # Check if strikes are < 4
+            for z in self.parachute:  # Print a live guy
                 print(z)
-
             print("   0   \n  /|\  \n  / \  \n\n^^^^^^^")
         else:
+            # print dead guy if strikes are > 4
             print("   X   \n  /|\  \n  / \  \n\n^^^^^^^")
