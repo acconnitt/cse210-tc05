a981b93eb8c41989acfa99ded941ae4faa28863e
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-05-21 20:13:16 -0400

    Output

--- jumper/game/output.py
+++ jumper/game/output.py
@@ -1,16 +1,13 @@
-
-
-
 class Output:
 
     def print_jumper(self, strikes, jumper):
-        jumper.print_jumper(strikes)
+        jumper.print_jumper(strikes) # Print the jumper figure
 
     def print_blanks(self, word):
-        print(*word.blanks, sep=" ")
+        print(*word.blanks, sep=" ")#Print blanks lines and space
 
     def print_win(self):
-        print("Congrats, you guessed the word!")
+        print("Congrats, you guessed the word!")#Print when user guesses all letter correctly
 
     def print_loss(self):
-        print("Sorry, you hit the ground a little too hard!")
+        print("Sorry, you hit the ground a little too hard!")#Print when user lose after 4 tries
